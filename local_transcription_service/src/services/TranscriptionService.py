9ce29ba1591a6a4e6aff409309fa625c193bf34b
Deeplerg <deeplerg@gmail.com>
2025-03-21 03:51:02 +0500

    Remove each audio chunk file right after transcribing it

--- local_transcription_service/src/services/TranscriptionService.py
+++ local_transcription_service/src/services/TranscriptionService.py
@@ -11,8 +11,6 @@ class TranscriptionService:
     def transcribe(self, file_path, segment_length_ms) -> list[TranscriptionChunk]:
         transcription_chunks = []
 
-        chunk_files = []
-
         for audio_chunk in split_audio(file_path, segment_length_ms, use_temp_dir=True):
             transcription = self.model.transcribe(audio_chunk.chunk_path)
 
@@ -23,12 +21,8 @@ class TranscriptionService:
             )
 
             transcription_chunks.append(chunk)
-            chunk_files.append(audio_chunk.chunk_path)
 
-        self.__cleanup_files(chunk_files)
+            if os.path.exists(audio_chunk.chunk_path):
+                os.remove(audio_chunk.chunk_path)
+
         return transcription_chunks
-
-    def __cleanup_files(self, files: list[str]):
-        for file_path in files:
-            if os.path.exists(file_path):
-                os.remove(file_path)
